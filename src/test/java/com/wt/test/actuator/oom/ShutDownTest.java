24109aa1a9f13818ca6085d0446819c5b6cee650
yipin.wt <yipin.wt@cfuture.shop>
2021-08-13 12:02:13 +0800

    初次提交 by qiyu 2021-08-13

---
+++ src/test/java/com/wt/test/actuator/oom/ShutDownTest.java
@@ -0,0 +1,38 @@
+package com.wt.test.actuator.oom;
+
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 一贫
+ * @date 2021/8/13
+ */
+public class ShutDownTest {
+
+    public static void main(String[] args) throws Exception {
+        Signal signal = new Signal(getOSSignalType());
+        Signal.handle(signal, (sig) -> {
+            Thread hook = new Thread(() -> {
+                System.out.println("ShutdownHook execute start...");
+                try {
+                    TimeUnit.SECONDS.sleep(2);//模拟应用进程退出前的处理操作
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("ShutdownHook execute end...");
+            }, "ShutDown Hook");
+            Runtime.getRuntime().addShutdownHook(hook);
+            Runtime.getRuntime().exit(0);
+        });
+        while (true) {
+            System.out.println("Program run");
+            TimeUnit.SECONDS.sleep(2);
+        }
+    }
+
+    private static String getOSSignalType() {
+        return System.getProperty("os.name").toLowerCase().startsWith("win") ? "INT" : "USR2";
+    }
+}
